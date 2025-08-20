c0e2a5a68678b3ffd5cc6c58b586810e01618fb8
DevalSavani <devalsavani30@gmail.com>
2025-08-20 22:30:50 +0530

    Add files via upload

---
+++ kb mb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    double bytes, kb, mb, gb;
+
+    printf("Enter size in bytes: ");
+    scanf("%lf", &bytes);
+
+    kb = bytes / 1024.0;                
+    mb = bytes / (1024.0 * 1024.0);    
+    gb = bytes / (1024.0 * 1024.0 * 1024.0); 
+
+    printf("\n%.2lf Bytes = %.2lf KB\n", bytes, kb);
+    printf("%.2lf Bytes = %.2lf MB\n", bytes, mb);
+    printf("%.2lf Bytes = %.2lf GB\n", bytes, gb);
+
+    return 0;
+}
