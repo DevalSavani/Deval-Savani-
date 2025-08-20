308d5ad8629504c5da69e10a2bd58f6d620bde4f
DevalSavani <devalsavani30@gmail.com>
2025-08-20 22:27:30 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+
+    float f, c;
+    printf("Enter the value of f: ");
+    scanf("%f",&f);
+    c = (f - 32)*5/9;
+    printf("c:%f ",c);
+    return 0;
+
+
+}
+
+
+
