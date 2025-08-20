d080d2fbf6b47978a3a7a2aec5069315c4ac294e
DevalSavani <devalsavani30@gmail.com>
2025-08-20 22:26:35 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, Sub;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    sum = num1 - num2;
+
+    printf("Sub = %.2lf\n", sub);
+
+    return 0;
+}
