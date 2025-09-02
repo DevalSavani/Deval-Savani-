7a9a79aeb421192138bf0e78e8e54f469ac5544a
DevalSavani <devalsavani30@gmail.com>
2025-09-02 21:58:09 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+    int main(){
+
+int values, sum = 0, num;
+float mean;
+printf("enter the values");
+scanf("%d\n",&values);
+for(int i=1; i<=values; i++){
+    printf("enter the numbers\n",i);
+    scanf("%d",&num);
+    sum = sum + number;
+
+
+
+
+}
+mean = sum / values;
+printf("your sum is %d \n", sum);
+printf("your mean is %f \n", mean);
+return 0;
+
+    }
